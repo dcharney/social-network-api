1d692ee375c78cbfc80c61eb2e27a7e259514283
dcharney <delaneycharney@gmail.com>
2021-03-06 10:33:21 +0100

    thought model complete

---
+++ controllers/thought-controller.js
@@ -0,0 +1,98 @@
+const { User } = require('../models');
+
+const userController = {
+    // //BONUS: remove user thoughts when deleted
+    // getAllUsers(req, res) {
+    //     User.find({})
+    //         .then(dbUserData => res.json(dbUserData))
+    //         .catch(err => res.status(400).json(err));
+    // },
+    // getUserById({ params }, res) {
+    //     User.findOne({ _id: params.id })
+    //         .populate({
+    //             path: 'friends',
+    //             select: '-__v'
+    //         })
+    //         .then(dbUserData => {
+    //             if (!dbUserData) {
+    //                 res.status(404).json({ message: "Invalid user id"});
+    //                 return;
+    //             }
+    //             res.json(dbUserData);
+    //         })
+    //         .catch(err => res.status(400).json(err));
+    // },
+    // // createUser,
+    // createUser({ body }, res) {
+    //     User.create(body)
+    //         .then(dbUserData => res.json(dbUserData))
+    //         .catch(err => res.status(400).json(err));
+    // },
+    // // updateUser,
+    // updateUser({ params, body}, res) {
+    //     User.findOneAndUpdate(
+    //             { _id: params.id }, 
+    //             body, 
+    //             { new: true, runValidators: true }
+    //         )
+    //         .then(dbUserData => {
+    //             if (!dbUserData) {
+    //                 res.status(404).json({ message: "invalid user id"});
+    //                 return;
+    //             }
+    //             res.json(dbUserData);
+    //         })
+    //         .catch(err => res.status(400).json(err));
+    // },
+    // // deleteUser
+    // deleteUser({ params }, res) {
+    //     User.findOneAndDelete({ _id: params.id })
+    //         .then(dbUserData => {
+    //             if (!dbUserData) {
+    //                 res.status(404).json({ message: "invalid user id"});
+    //                 return;
+    //             }
+    //             res.json(dbUserData);
+    //         })
+    //         .catch(err => res.status(400).json(err));
+    // },
+    // // add friend to friends list
+    // addFriend({ params }, res) {
+    //     User.findOneAndUpdate(
+    //         { _id: params.userId },
+    //         { $push: { friends: params.friendId } },
+    //         { new: true}
+    //     )
+    //         .then(dbUserData => {
+    //             if (!dbUserData) {
+    //                 res.status(404).json({ message: 'invalid user id' })
+    //             }
+    //             res.json(dbUserData);
+    //         })
+    //         .catch(err => res.json(err));
+    // },
+    // // remove friend from friends list
+    // removeFriend({ params }, res) {
+    //     User.findOneAndUpdate(
+    //         { _id: params.userId },
+    //         { $pull: { friends:params.friendId } },
+    //         { new: true}
+    //     )
+    //         .then(dbUserData => {
+    //             if (!dbUserData) {
+    //                 res.status(404).json({ message: 'invalid user id' })
+    //             }
+    //             res.json(dbUserData);
+    //         })
+    //         .catch(err => res.json(err));
+    // } 
+    getAllThoughts() {},
+    getThoughtById() {},
+    createThought() {},
+    updateThought() {},
+    deleteThought() {},
+    addReaction() {},
+    removeReaction() {}
+}
+
+module.exports = userController;
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+// Set up GET all and POST at /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+// Set up GET one, PUT, and DELETE at /api/thoughts/:thoughtId
+router
+    .route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// Set up POST friend at /api/thoughts/:thoughtId/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction)
+
+// Set up DELETE friend at /api/thoughts/:thoughtId/reactions/:readtionId
+router
+    .route('/:thoughtId/reactions/:readtionId')
+    .delete(removeReaction);
+
+module.exports = router;
